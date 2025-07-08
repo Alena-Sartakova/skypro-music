93832e1a645a914344eb336875bcd09cde96cc7b
Alena <sartakovaalenka@gmail.com>
2025-07-08 22:16:27 +0700

    стилизация без разбития на компоненты

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+import path from 'node:path' // Добавлен импорт path
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/components/img/icons')], // Путь к папке с иконками
-      symbolId: 'icon-[name]', // Формат ID иконки (например: icon-home)
-    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@': '/src',
-    },
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
-
